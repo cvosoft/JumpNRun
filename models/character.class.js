aca4c00b4571755548b6c5859083d6155d352b61
Christoph Völker <christoph@herrvoelker.de>
2024-09-02 17:24:52 +0200

    update

--- models/character.class.js
+++ models/character.class.js
@@ -1,7 +1,7 @@
 class Character extends MovableObject {
 
     height = 250;
-    y = 180;
+    y = 80;
     speed = 10;
     IMAGES_WALKING = [
         'img/2_character_pepe/2_walk/W-21.png',
@@ -11,6 +11,20 @@ class Character extends MovableObject {
         'img/2_character_pepe/2_walk/W-25.png',
         'img/2_character_pepe/2_walk/W-26.png',
     ];
+
+
+    IMAGES_JUMPING = [
+        'img/2_character_pepe/3_jump/J-31.png',
+        'img/2_character_pepe/3_jump/J-32.png',
+        'img/2_character_pepe/3_jump/J-33.png',
+        'img/2_character_pepe/3_jump/J-34.png',
+        'img/2_character_pepe/3_jump/J-35.png',
+        'img/2_character_pepe/3_jump/J-36.png',
+        'img/2_character_pepe/3_jump/J-37.png',
+        'img/2_character_pepe/3_jump/J-38.png',
+        'img/2_character_pepe/3_jump/J-39.png',
+    ]
+
     world;
     walking_sound = new Audio('./audio/walking.wav');
 
@@ -19,7 +33,8 @@ class Character extends MovableObject {
         super().loadImage('../img/2_character_pepe/1_idle/idle/I-1.png');
 
         this.loadImages(this.IMAGES_WALKING);
-
+        this.loadImages(this.IMAGES_JUMPING);
+        this.applyGravity();
         this.animate();
 
     }
@@ -29,32 +44,42 @@ class Character extends MovableObject {
         setInterval(() => {
             this.walking_sound.pause();
             if (this.world.keyboard.RIGHT && this.x < this.world.level.level_end_x) {
-                this.x += this.speed;
+                this.moveRight();
                 this.otherDirection = false;
                 this.walking_sound.play();
             }
             if (this.world.keyboard.LEFT && this.x > 0) {
-                this.x -= this.speed;
+                this.moveLeft();
                 this.otherDirection = true;
                 this.walking_sound.play();
             }
+
+            if (this.world.keyboard.SPACE && !this.isAboveGround()) {
+                this.jump();
+            }
+
+
             this.world.camera_x = -this.x + 100;
         }, 1000 / 60);
 
         setInterval(() => {
 
-            if (this.world.keyboard.RIGHT || this.world.keyboard.LEFT) {
-                // walk animaton
-                this.playAnimation(this.IMAGES_WALKING);
+            if (this.isAboveGround()) {
+                this.playAnimation(this.IMAGES_JUMPING);
+            } else {
 
+
+                if (this.world.keyboard.RIGHT || this.world.keyboard.LEFT) {
+                    // walk animaton
+                    this.playAnimation(this.IMAGES_WALKING);
+
+                }
             }
         }, 50);
     }
 
 
-    jump() {
 
-    }
 
 
 
--- models/movable-object.class.js
+++ models/movable-object.class.js
@@ -8,6 +8,8 @@ class MovableObject {
     imageCache = {};
     currentImage = 0;
     otherDirection = false;
+    speedY = 0;
+    acceleration = 2.5;
 
 
     loadImage(path) {
@@ -26,15 +28,12 @@ class MovableObject {
 
 
     moveRight() {
-        console.log("moving right");
+        this.x += this.speed;
     }
 
 
-
     moveLeft() {
-        setInterval(() => {
-            this.x -= this.speed;
-        }, 1000 / 60)
+        this.x -= this.speed;
     }
 
     playAnimation(images) {
@@ -43,5 +42,24 @@ class MovableObject {
         this.img = this.imageCache[path];
         this.currentImage++;
     }
+
+    applyGravity() {
+        setInterval(() => {
+            if (this.isAboveGround() || this.speedY > 0) {
+                this.y -= this.speedY;
+                this.speedY -= this.acceleration;
+            }
+        }, 1000 / 25);
+    }
+
+    isAboveGround() {
+        return this.y < 180;
+    }
+
+
+    jump() {
+        this.speedY = 30;
+    }
+
 
 }
