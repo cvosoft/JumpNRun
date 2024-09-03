0f595ffe047ddcf161e8ff4f22ae25f1067c1c33
Christoph Völker <christoph@herrvoelker.de>
2024-09-03 10:36:28 +0200

    update

--- models/movable-object.class.js
+++ models/movable-object.class.js
@@ -61,5 +61,16 @@ class MovableObject {
         this.speedY = 30;
     }
 
+    draw(ctx) {
+        ctx.drawImage(this.img, this.x, this.y, this.width, this.height);
+    }
+
+    drawFrame(ctx) {
+        ctx.beginPath();
+        ctx.lineWidth = '5';
+        ctx.strokeStyle = 'blue';
+        ctx.rect(this.x, this.y, this.width, this.height);
+        ctx.stroke();
+    }
 
 }
--- models/world.class.js
+++ models/world.class.js
@@ -53,16 +53,28 @@ class World {
     addToMap(mo) {
 
         if (mo.otherDirection) {
-            this.ctx.save();
-            this.ctx.translate(mo.width, 0);
-            this.ctx.scale(-1, 1);
-            mo.x = -mo.x;
+            this.flipImage(mo);
         }
-        this.ctx.drawImage(mo.img, mo.x, mo.y, mo.width, mo.height);
+        mo.draw(this.ctx);
+
+        mo.drawFrame(this.ctx);
+
+
         if (mo.otherDirection) {
-            mo.x = -mo.x;
-            this.ctx.restore();
+            this.flipImageBack(mo);
         }
+    }
+
+    flipImage(mo) {
+        this.ctx.save();
+        this.ctx.translate(mo.width, 0);
+        this.ctx.scale(-1, 1);
+        mo.x = -mo.x;
+    }
+
+    flipImageBack(mo) {
+        mo.x = -mo.x;
+        this.ctx.restore();
     }
 
 }
